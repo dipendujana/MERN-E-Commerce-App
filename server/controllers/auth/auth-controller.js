ed3d766ac166936a9058996fc7ad641960821c89
dipendujana5 <your-email@example.com>
2024-12-11 00:31:17 +0530

    third commit

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -60,10 +60,22 @@ const loginUser = async(req,res)=>{
     );
 
 
-    res.cookie('token',token,{httpOnly: true, secure : true}).json({
-      success : true,
+    // res.cookie('token',token,{httpOnly: true, secure : true}).json({
+    //   success : true,
+    //   message : 'Logged in successfully',
+    //   user : {
+    //     email : checkUser.email,
+    //     role : checkUser.role,
+    //     id : checkUser._id,
+    //     userName: checkUser.userName,
+    //   }
+    // })
+
+    res.status(200).json({
+      success :true,
       message : 'Logged in successfully',
-      user : {
+      token,
+        user : {
         email : checkUser.email,
         role : checkUser.role,
         id : checkUser._id,
@@ -96,8 +108,26 @@ const logoutUser = (req,res)=> {
 
 //auth middleware
 
+// const authMiddleware = async(req,res,next)=>{
+//   const token = req.cookies.token;
+//   if(!token) return res.status(401).json({
+//     success: false,
+//     message: 'Unauthorized user!'
+//   })
+//   try{
+//     const decoded = jwt.verify(token,'CLIENT_SECRET_KEY');
+//     req .user= decoded;
+//     next()
+//   } catch(error){
+//     res.status(401).json({
+//       success: false,
+//       message: 'Unauthorized user!'
+//     })
+//   }
+// }
 const authMiddleware = async(req,res,next)=>{
-  const token = req.cookies.token;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]
   if(!token) return res.status(401).json({
     success: false,
     message: 'Unauthorized user!'
